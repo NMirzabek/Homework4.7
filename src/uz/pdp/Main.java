53f08cd7b87d274f9c8bf0c62eb451135ff55545
Mirzabek <mirzabeknormakhmatov03@gmail.com>
2024-12-15 14:33:15 +0500

    4 - qator

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -3,5 +3,7 @@ package uz.pdp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1 - qator");
+        System.out.println("2 - qator");
+        System.out.println("3 - qator");
     }
 }
