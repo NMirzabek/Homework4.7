be2cb7f3e26a505cba8b730a5c3324ce4a59cea1
Mirzabek <mirzabeknormakhmatov03@gmail.com>
2024-12-15 14:33:46 +0500

    5 - qator

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("1 - qator");
         System.out.println("2 - qator");
         System.out.println("3 - qator");
+        System.out.println("4 - qator");
     }
 }
