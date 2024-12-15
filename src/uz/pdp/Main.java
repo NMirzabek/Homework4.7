717488e818643d75d53534682be7cdeefd13d922
Mirzabek <mirzabeknormakhmatov03@gmail.com>
2024-12-15 14:34:41 +0500

    7-qator

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("3 - qator");
         System.out.println("4 - qator");
         System.out.println("5 - qator");
+        System.out.println("6 - qator");
     }
 }
